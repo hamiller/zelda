1b016721c60a563a628e8efa825ce54ea18c274b
Florian Miess <florian.miess@innoq.com>
2022-02-10 11:54:53 +0100

    Erste Schritte

---
+++ entities/Player.gd
@@ -0,0 +1,43 @@
+extends KinematicBody2D
+
+const MAX_SPEED = 200
+signal habe_gehauen
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print("Ich existiere!")
+	
+
+func _physics_process(_delta):
+	var vector = Vector2.ZERO
+	if Input.is_action_pressed("up_pressed"):
+		vector.y = -1
+	if Input.is_action_pressed("down_pressed"):
+		vector.y = 1
+	if Input.is_action_pressed("left_pressed"):
+		vector.x = -1
+	if Input.is_action_pressed("right_pressed"):
+		vector.x = 1
+	
+	vector = vector.normalized()
+	
+	animate(vector.x, vector.y)
+	move_and_slide(vector * MAX_SPEED)
+	
+	
+func _input(event):
+	if event.is_action_pressed("hack_and_slay"):
+		print("killlll!!!!")
+		emit_signal("habe_gehauen")
+
+
+func animate(x, y):
+	if x == 1:
+		$AnimatedSprite.set_animation("move_right")
+	if x == -1:
+		$AnimatedSprite.set_animation("move_left")
+	if y == 1:
+		$AnimatedSprite.set_animation("move_down")
+	if y == -1:
+		$AnimatedSprite.set_animation("move_up")
+
